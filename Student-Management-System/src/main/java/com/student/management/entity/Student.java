6933ae02ccf92f52f4b6d8fe8406eed16f4b573b
vijayhandal <handalvijay9588@gmail.com>
2023-01-23 20:46:15 +0530

    update

--- Student-Management-System/src/main/java/com/student/management/entity/Student.java
+++ Student-Management-System/src/main/java/com/student/management/entity/Student.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,6 +31,7 @@ public class Student {
 	@Column(name = "email")
 	private String email;
 
+	@NotNull
 	private String address;
 	
 	public Student(String firstName, String lastName, String email) {
