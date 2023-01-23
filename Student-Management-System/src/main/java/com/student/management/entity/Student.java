af4278092105619998bdd9381c43e842ffa7349c
vijayhandal <handalvijay9588@gmail.com>
2023-01-23 20:49:14 +0530

    update

--- Student-Management-System/src/main/java/com/student/management/entity/Student.java
+++ Student-Management-System/src/main/java/com/student/management/entity/Student.java
@@ -10,8 +10,8 @@ import javax.persistence.Table;
 
 // import lombok.AllArgsConstructor;
 import lombok.Data;
-// import lombok.NoArgsConstructor;
-// import lombok.ToString;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 // @AllArgsConstructor
