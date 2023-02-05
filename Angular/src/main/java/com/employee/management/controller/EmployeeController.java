c3ce5fe4c951b2273e09358a3243b77be4707772
vijayhandal <handalvijay9588@gmail.com>
2023-02-05 20:51:57 +0530

    update

--- Angular/src/main/java/com/employee/management/controller/EmployeeController.java
+++ Angular/src/main/java/com/employee/management/controller/EmployeeController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.employee.management.model.Employee;
 import com.employee.management.repository.EmployeeRepository;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,5 +23,9 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return empRepo.findAll();
+	}
+	@PostMapping("/employees")
+	public Employee addNewEmployee(@RequestBody Employee emp){
+		return empRepo.save(emp);
 	}
 }
