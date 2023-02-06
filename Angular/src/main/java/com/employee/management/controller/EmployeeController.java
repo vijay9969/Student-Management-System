904f2e230a8bb096327a9dcc1d1922019ee9a995
vijayhandal <handalvijay9588@gmail.com>
2023-02-06 20:05:33 +0530

    update

--- Angular/src/main/java/com/employee/management/controller/EmployeeController.java
+++ Angular/src/main/java/com/employee/management/controller/EmployeeController.java
@@ -3,7 +3,9 @@ package com.employee.management.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,10 @@ public class EmployeeController {
 	@PostMapping("/employees")
 	public Employee addNewEmployee(@RequestBody Employee emp){
 		return empRepo.save(emp);
+	}
+	@DeleteMapping("/employees/{id}")
+	public String deleteEmployee(@PathVariable("id") Long id){
+		 empRepo.deleteById(id);
+		 return "Employee deleted";
 	}
 }
